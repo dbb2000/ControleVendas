dfe8adc5a5a0fabec6b0ab542b652cebd9b66af9
dbb2000 <dbb2000@gmail.com>
2017-04-20 18:58:08 -0300

    geração de relatório de lista de produtos do revendedor finalmente implementado e com código refatorado

--- src/main/java/br/com/jade/report/GeradorDeRelatorios.java
+++ src/main/java/br/com/jade/report/GeradorDeRelatorios.java
@@ -1,61 +1,48 @@
 package br.com.jade.report;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.util.Map;
 
-import javax.faces.context.FacesContext;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.view.JasperViewer;
+import net.sf.jasperreports.engine.JasperRunManager;
 
-public class GeradorDeRelatorios extends HttpServlet {
+public class GeradorDeRelatorios {
 
-//    private Connection conexao;
-//
-//    public GeradorDeRelatorios(Connection conexao) {
-//        this.conexao = conexao;
-//    }
-//
-//    public void geraPdf(String jrxml, Map<String, Object> parametros) {
-//
-//        try {
-//
-//            // compila jrxml em memoria
-//        	String absolutePath= FacesContext.getCurrentInstance().getExternalContext().getRealPath(jrxml);
-//            JasperReport jasper = JasperCompileManager.compileReport(absolutePath);
-//
-//            // preenche relatorio
-//            JasperPrint print = JasperFillManager.fillReport(jasper, parametros, this.conexao);
-//           // JasperViewer.viewReport(print, false); // exibe na tela para impressão ou salvar (melhor solução)
-//           JasperViewer jasperViewer = new JasperViewer(print);
-//           jasperViewer.setVisible(true);
-//            
-////             exporta para pdf (não preciso agora mas vou deixar comentado
-////            JRExporter exporter = new JRPdfExporter();
-////            exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
-////            exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, saida);
-////            exporter.exportReport();
-//
-//        } catch (Exception e) {
-//            throw new RuntimeException("Erro ao gerar relatório", e);
-//        }
-//    }   
-	
-	public void lala ( HttpServletResponse response, byte[] bytes) throws IOException{
-		response.reset();
-	    response.resetBuffer();
-	    response.setContentType("application/pdf");
-	    response.setContentLength(bytes.length);
-	    ServletOutputStream ouputStream = response.getOutputStream();
-		ouputStream.write(bytes, 0, bytes.length);
-		ouputStream.flush();
-		ouputStream.close();
+	private Connection conexao;
+
+	public GeradorDeRelatorios(Connection conexao) {
+		this.conexao = conexao;
 	}
+
+	public void geraPdf(Map<String, Object> parametros, HttpServletResponse response, String absolutePath) throws JRException {
+
+		try {
+			JasperReport jasper = JasperCompileManager.compileReport(absolutePath);
+			Charset latin1Charset = Charset.forName("ISO-8859-1");
+			CharBuffer charBuffer = latin1Charset.decode(ByteBuffer.wrap(JasperRunManager.runReportToPdf(jasper, parametros, conexao)));
+			char[] chars= charBuffer.array();
+
+			response.reset();
+			response.resetBuffer();
+			response.setContentType("application/pdf");
+			response.setContentLength(chars.length);
+			
+			PrintWriter printWriter = response.getWriter();
+			printWriter.write(chars, 0, chars.length);
+			printWriter.flush();
+			printWriter.close();
+
+		} catch (IOException e) {
+			throw new RuntimeException("Erro ao gerar relatório", e);
+		}
+	}   
 }
