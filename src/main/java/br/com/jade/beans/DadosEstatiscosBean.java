e5f98fc49d732e86a970f9444d2c6e8dca01e2a7
dbb2000 <dbb2000@gmail.com>
2017-04-26 19:33:20 -0300

    inicio do desenvolvimento da tela de estatísticas e dados financeiros

---
+++ src/main/java/br/com/jade/beans/DadosEstatiscosBean.java
@@ -0,0 +1,10 @@
+package br.com.jade.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class DadosEstatiscosBean {
+
+}
---
+++ src/main/java/br/com/jade/dao/EstatisticoDao.java
@@ -0,0 +1,16 @@
+package br.com.jade.dao;
+
+import java.io.Serializable;
+
+import javax.persistence.EntityManager;
+
+import br.com.jade.util.JpaUtil;
+
+public class EstatisticoDao implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2410045853541510854L;
+	private EntityManager manager = JpaUtil.getEntityManager();
+}
